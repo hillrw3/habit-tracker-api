631ecd3f1f3730e08c2c581c424b94f5b8266720
Rob Hill <robert.hill0122@gmail.com>
2017-12-04 19:55:48 -0700

    Deliver habit reminder emails asynchronously

--- app/services/habit_reporter_service.rb
+++ app/services/habit_reporter_service.rb
@@ -4,7 +4,7 @@ class HabitReporterService
     User.all.includes(:current_habits).each do |user|
       unfinished_habits = user.current_habits.to_a.keep_if(&:unfinished?)
 
-      HabitMailer.reminder(user.email, unfinished_habits).deliver_now
+      HabitMailer.reminder(user.email, unfinished_habits).deliver_later
     end
   end
 end
--- spec/services/habit_reporter_service_spec.rb
+++ spec/services/habit_reporter_service_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
+include ActiveJob::TestHelper
 
 describe HabitReporterService do
-  describe '.sendreminder' do
+  describe '.send_reminder' do
     it 'sends an email with a list of unfinished habits for the current week' do
       user = create_user(email: 'bill@blah.com')
       finished_habit = create_habit(title: 'look how finished i am', user_id: user.id, target_frequency: 1, actual_frequency: 1)
       unfinished_habit = create_habit(title: 'man, im unfinished', user_id: user.id, target_frequency: 3, actual_frequency: 1)
       old_unfinished_habit = create_habit(title: 'man, im unfinished AND old', user_id: user.id, created_at: 1.month.ago)
 
-      expect { HabitReporterService.send_reminder }.to change { ActionMailer::Base.deliveries.count }.by(1)
+      expect {
+        perform_enqueued_jobs do
+          HabitReporterService.send_reminder
+        end
+      }.to change { ActionMailer::Base.deliveries.size }.by(1)
 
       email = ActionMailer::Base.deliveries.last
 
