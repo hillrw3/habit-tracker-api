2e68480837d086c8c6f2d02d10b95c97c43fd5c9
Rob Hill <rob@Robs-MacBook-Pro.local>
2018-01-16 19:15:30 -0700

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,6 @@ ActiveRecord::Schema.define(version: 20171202191424) do
     t.index ["user_id"], name: "index_habits_on_user_id"
   end
 
-  create_table "super_habits", force: :cascade do |t|
-    t.integer "habit_id"
-    t.integer "child_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
